2c9e4ca881b59833f6282126454c103e14c5e65f
zamran <family_zj@yahoo.com>
2017-12-06 16:28:20 -0600

    framework at work

---
+++ CoreHybridFramwork/CoreHybridFramwork/GenericKeywords.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreHybridFramwork
+{
+    class GenericKeywords
+    {
+        //open browser
+        //navigate
+        //click somewhere
+        //input the data- type
+        //verify Title 
+        //verify text
+
+    }
+}
---
+++ CoreHybridFramwork/CoreHybridFramwork/GmailLogin.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreHybridFramwork
+{
+    [TestFixture]
+    public class GmailLogin
+    {
+        [Test]
+        public void DoLogin()
+        {
+            //webdriver code
+            //validate the presence of object
+
+            //hardcode - no
+            //read from xls file
+
+            //able to run on different browsers
+
+            //configuration file - locators 
+            //driver.FindElement(By.Xpath())
+        }
+    }
+}
